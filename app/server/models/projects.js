8497f8d37c46a280b710031c368b14c7f9e805eb
aqueous-humor <akpos97@ymail.com>
2021-04-23 17:04:21 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -15,7 +15,7 @@ class Projects extends DataModel {
     validate(obj) {
        this.errors = [];
        for (let key in obj) {
-           if (obj[key] == '' || obj[key] == []) {
+           if (obj[key] == '' || obj[key] == [] || obj[key] == null) {
               this.errors.push(`${key} should not be empty`);
             }
        }
